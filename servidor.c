47a8c81d85baec86f7fde3b597c5a32e237b979a
Felipe Junqueira Leite <felipejleite18@gmail.com>
2026-02-06 15:16:22 -0300

    Update servidor.c
    Atualizamos com algumas correções de bugs. Ainda tem erros a serem corrigidos na leitura do curl, mas a mensagem já está sendo enviada. Agora o curl recebe uma mensagem decente via http.

--- servidor.c
+++ servidor.c
@@ -6,6 +6,8 @@
 
 // Socket conecta IP, Porta e Protocolo(TCP)
 // Para compilar gcc servidor.c -o servidor -lws2_32
+// .\servidor.exe
+// Em outro terminal, escrever curl localhost:8080 para fazer a conexao
 
 int main(){
     // O código cria um servidor que escuta na porta 8080, aceita uma conexão, envia uma mensagem e fecha.
@@ -16,6 +18,7 @@ int main(){
         printf("Falha. Erro: %d", WSAGetLastError());
         return 1;
     }
+    else printf("Ambiente Inicializado\n");
 
     // socket(Familia de endereco, tipo, protocolo)
     // AF_INET: IPv4; 
@@ -26,6 +29,7 @@ int main(){
     if(s == INVALID_SOCKET) {
         printf("Nao foi possivel criar o socket");
     }
+    else printf("Socket criado\n");
 
     struct sockaddr_in server; // Estrutura para configurar o IP e porta
     server.sin_family = AF_INET; // Define que usaremos IPv4
@@ -34,24 +38,53 @@ int main(){
 
     // Ritual de conexao: Bind, Listen e Accept
     // Bind associa o socket 's' as configuracoes de ip/porta criadas
-    bind(s, (struct sockaddr *)&server, sizeof(server)); // Casting do endereco de server
+    if(bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+        printf("Erro no bind: %d", WSAGetLastError()); // Casting do endereco de server
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+    else printf("Blind feito!\n");
 
     // Listen coloca o socket em mode de espera. '3' eh o tamanho da fila de espera
     listen(s, 3);
+    printf("Servidor iniciado. Aguardando conexao na porta 8080...\n");
 
     // Accept o programa "para" aqui e fica esperando alguem conectar
     // Quando alguem conecta, ele cria um NOVO socket (novo_socket) so para falar com esse cliente especifico
     int c = sizeof(struct sockaddr_in);
     struct sockaddr_in client;
     SOCKET novo_socket = accept(s, (struct sockaddr *)&client, &c);
+    if (novo_socket == INVALID_SOCKET) {
+        printf("Novo socket nao criado\n %d", WSAGetLastError());
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+    else printf("Novo socket criado!\n");
 
-    // Agora a comunicacao
-    char *msg = "Ola! voce se conectou.\n";
+    char msg[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nOla! voce se conectou.\n";
+    int size = sizeof(msg) - 1; // Tiramos o '\0';
 
     // Send envia os bytes da string atraves do socket do cliente
-    send(novo_socket, msg, strlen(msg), 0);
+    int resultado_envio = send(novo_socket, msg, size, 0);
+    if (resultado_envio == SOCKET_ERROR) {
+        printf("Falha no envio da mensagem. Codigo de erro: %d\n", WSAGetLastError());
+        closesocket(s);
+        closesocket(novo_socket);
+        WSACleanup();
+        return 1;
+    } 
+    else {
+        printf("Mensagem enviada com sucesso. %d bytes transferidos :)\n", resultado_envio );
+        // shutdown(socket, SD_SEND) avisa o termino ao cliente
+        // Isso permite que o curl finalize a recepcao corretamente antes de fechar o socket
+        shutdown(novo_socket, SD_SEND);
+    }
+
 
     // Limpeza obrigatoria para nao deixar lixo na memoria/rede
+    closesocket(novo_socket); // Fecha a conexao com o cliente
     closesocket(s); // Fecha o socket principal
     WSACleanup(); // Desliga a bib Winsock
 
